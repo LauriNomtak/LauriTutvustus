8a361216ab035b5232612540e5ca0ca6a95d3167
allarkikkas <allar.kikkas@khk.ee>
2015-09-16 16:16:19 +0300

    Esilehele lisatud Facebooki Like nupp [Delivers #102884908]

--- index.php
+++ index.php
@@ -5,10 +5,24 @@
     <title>Document</title>
 </head>
 <body>
+
+<div id="fb-root"></div>
+<script>(function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
 <dl>
     <dt>Lemmikspordiala</dt>
     <dd>Jalgpall</dd>
 </dl>
+
+<div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="standard" data-action="like"
+     data-show-faces="true" data-share="true"></div>
 
 </body>
 </html>
