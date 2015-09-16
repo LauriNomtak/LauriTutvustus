a98dab291ab74ab2d2a1531d2b6c2d2415840bff
allarkikkas <allar.kikkas@khk.ee>
2015-09-16 16:21:42 +0300

    Lisatud veel veidi fakte ja pilt [Delivers #102647908]

--- index.php
+++ index.php
@@ -19,10 +19,26 @@
 <dl>
     <dt>Lemmikspordiala</dt>
     <dd>Jalgpall</dd>
+
+    <dt>Nimi</dt>
+    <dd>Lauri Nõmtak</dd>
+
+    <dt>Vanus</dt>
+    <dd>25</dd>
+
+    <dt>Sünnikoht</dt>
+    <dd>Rakvere</dd>
+
+    <dt>Hobid</dt>
+    <dd>Muusikaga tegelemine, jooksmine, lugemine.</dd>
+
 </dl>
 
-<div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="standard" data-action="like"
+<div class="fb-like" data-href="http://developers.facebook.com/docs/plugins/" data-layout="standard" data-action="like"
      data-show-faces="true" data-share="true"></div>
 
+<img
+    src="http://fbcdn-photos-a-a.akamaihd.net/hphotos-ak-xaf1/v/t1.0-0/p480x480/424462_371389616212572_938012550_n.jpg?oh=7ac041bd2437b8e7015e2e036c8ea452&oe=56A71AF7&__gda__=1450860456_e2d82a61549f345ddc9fd2f5a75bb304"
+    alt="Lauri">
 </body>
 </html>
