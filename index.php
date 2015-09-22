f9ea17d983abcfa4b59e7c3ce457d31a0a4e004c
allarkikkas <allar.kikkas@khk.ee>
2015-09-22 08:50:22 +0300

    Lisatud css fail (work in progress) [#102647908]

--- index.php
+++ index.php
@@ -1,45 +1,92 @@
 <!doctype html>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
+    <link type="text/css" rel="stylesheet" href="css.css"/>
+
     <meta charset="UTF-8">
     <title>Document</title>
-    <link rel="stylesheet" href="default.css">
-</head>
+<head>
+    <style>
+
+
+        h1 {
+            background-color:;
+        }
+
+        p {
+            background-color:;
+        }
+    </style>
+
+
 <body>
 
-<div id="fb-root"></div>
-<script>(function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
-
-<dl>
-    <dt>Lemmikspordiala</dt>
-    <dd>Jalgpall</dd>
-
-    <dt>Nimi</dt>
-    <dd>Lauri Nõmtak</dd>
-
-    <dt>Vanus</dt>
-    <dd>25</dd>
-
-    <dt>Sünnikoht</dt>
-    <dd>Rakvere</dd>
-
-    <dt>Hobid</dt>
-    <dd>Muusikaga tegelemine, jooksmine, lugemine.</dd>
-
-</dl>
-
-<div class="fb-like" data-href="http://developers.facebook.com/docs/plugins/" data-layout="standard" data-action="like"
-     data-show-faces="true" data-share="true"></div>
-
 <img
-    src="http://fbcdn-photos-a-a.akamaihd.net/hphotos-ak-xaf1/v/t1.0-0/p480x480/424462_371389616212572_938012550_n.jpg?oh=7ac041bd2437b8e7015e2e036c8ea452&oe=56A71AF7&__gda__=1450860456_e2d82a61549f345ddc9fd2f5a75bb304"
-    alt="Lauri">
+    src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xaf1/v/t1.0-1/c191.41.517.517/424462_371389616212572_938012550_n.jpg?oh=f346f26b66547cb2af8a37698036136d&oe=565FE6D3&__gda__=1449455479_9c885111880e4f84f788f98021256507"
+    alt="W3Schools.com" style="width:500px;height:300px;">
+
 </body>
+
+
+</head>
+<font color="white">
+    <head><p><b><font size="40"
+                      style="border: 5px solid; border-style:solid; border-color:orange; border-radius:50px; box-shadow: 5px 5px 5px #888888;;">Lauri
+                    enesetuvustus</font></b></p></head>
+
+    <hr>
+
+
+    <h2><a href="https://www.facebook.com/lauri.nomtak?fref=ts">‎Lauri Nõmtak Facebook Profile</a></h2>
+
+
+    <div id="one"
+
+
+    <h1>VS15</h1>
+    <table style="border: solid white;width:30% ">
+        <tr>
+            <th style="text-align: left">Nimi</th>
+            <td>Vanus</td>
+            <td>Sünnikoht</td>
+        </tr>
+        <tr>
+            <td>Lauri Nõmtak</td>
+            <td>25</td>
+            <td>Rakvere</td>
+        </tr>
+
+        </tr>
+    </table>
+
+    <!--teine asi-->
+    <div id="two"
+    <h2>Hobid</h2>
+
+    <li>Muusikagategelemine</li>
+    <li>jooksmine</li>
+    <li>lugemine</li>
+    <li>lemmik spordiala: Jalgpall</li>
+    </body></font>
+
+</div>
+
+<br>
+
+<div id="kolm"
+<h3>Lemmik Filmid</h3>
+<ul>
+    <li>Bad Boys 3</li>
+    <li>American Ultra</li>
+</ul>
+
+</div>
+
+
+<div id="pilt"
+<img src="http://giphy.com/gifs/funny-smile-titanic-kSPWeYDJ0rrq0" alt="titanic" height="42" width="42">
+
+</di>
+
 </html>
+
